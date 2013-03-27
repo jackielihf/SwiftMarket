8f255f98165c8c4b1125fb8585ecb6bde0dd2ddd
jackielihf <jackie.lihf@gmail.com>
2013-03-27 16:21:35 +0800

    first commit

---
+++ DataSimulator/src/sim/Simulator.java
@@ -0,0 +1,28 @@
+/**
+ * Copy Right Reserve
+ * Project:
+ * JDK version: jdk1.7.0
+ * Version: 1.0
+ * Modification history:
+ * @author jackie
+ */
+package sim;
+
+import java.io.*;
+
+
+/**
+ * @author jackie
+ * Description: data simulator, can be used to generate real-time data of stock market.
+ */
+public class Simulator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("data simulator started...");
+
+	}
+
+}
